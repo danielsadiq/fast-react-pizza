a309d7ca2b4dd951046df4243b0f37978a21c9b9
Daniel Sadiq <danielsadiq93@gmail.com>
2025-05-28 15:31:27 +0100

    style update

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -3,10 +3,10 @@ import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
-    <header>
+    <header className="bg-yellow-500 px-4 py-3 uppercase border-b border-stone-500 sm:px-6">
       <Link to="/">Fast React Pizza Co.</Link>
       <SearchOrder />
-      <p>Daniel</p>
+      <p className="hidden">Daniel</p>
     </header>
   );
 }
