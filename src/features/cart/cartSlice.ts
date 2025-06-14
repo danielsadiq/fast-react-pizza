6045c6f1e29a45dfeef8ef3894e11b61ad419607
Daniel Sadiq <danielsadiq93@gmail.com>
2025-06-14 13:43:37 +0100

    cart slice errors remove

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,17 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit"
 import type { CartItemType } from "../../types/cart"
 
-const initialState = {
-  // cart: [],
-  cart: [
-    {
-      pizzaId: 12,
-      name: 'Mediterranean',
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32
-    },
-  ],
+interface InitialStateType {
+  cart: CartItemType[]; 
+}
+const initialState: InitialStateType = {
+  cart: [],
+  
+  // cart: [
+  //   {
+  //     pizzaId: 12,
+  //     name: 'Mediterranean',
+  //     quantity: 2,
+  //     unitPrice: 16,
+  //     totalPrice: 32
+  //   },
+  // ],
 }
 
 const cartSlice = createSlice({
@@ -48,6 +52,10 @@ const cartSlice = createSlice({
 
 export const {addItem, deleteItem, increaseItemQuantity, decreaseItemQuantity, clearCart} = cartSlice.actions
 export default cartSlice.reducer
+
+export const getCart = ((state: { cart: { cart: CartItemType[] } }) => state.cart.cart);
+
+export const getUserName = (state: { user: {username:string}}) => state.user.username;
 
 export const getTotalCartQuantity =  (state: { cart: { cart: CartItemType[] } }) => state.cart.cart.reduce((sum:number,item:CartItemType)=> sum + item.quantity, 0)
 
